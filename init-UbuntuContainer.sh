cfb55fdc3e97978d3d193b87520df65fea5b130d
Arellano1904 <arellano1904@hotmail.com>
2025-07-01 10:26:29 -0600

    Archivos para crear el contenedor del proyecto.

---
+++ init-UbuntuContainer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Terminamos el proceso en caso de haber algún error:
+set -e
+
+# Mensajes para el log de inicio.
+echo "***** Iniciando Contendor *****"
+echo "*** Version de Python ***"
+python3 --version
+echo "*** Version de pip ***"
+pip --version
+echo "*****   Contenedor iniciado correctamente   *****"
+echo "Para iniciar el servidor de fastapi, ejecute: fastapi dev main.py --host 0.0.0.0 --port 8000"
+
+# Mantenemos el contenedor activo.
+tail -f /dev/null
